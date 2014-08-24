da19625057150d0d09deada0e334c15ac4e8c68a
Rahazan <guido.reaver@gmail.com>
2014-08-25 01:42:26 +0200

    Added timescale controls

---
+++ LD30/Assets/Scripts/SpeedManipulation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedManipulation : MonoBehaviour {
+
+    public TextMesh textMesh;
+
+
+	// Use this for initialization
+	void Start () {
+        Time.timeScale = 1f;
+        textMesh = GetComponent<TextMesh>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Equals) || Input.GetKeyDown(KeyCode.Plus) || Input.GetKeyDown(KeyCode.KeypadPlus))
+        {
+            Time.timeScale += 0.1f;
+        }
+        if (Input.GetKeyDown(KeyCode.Minus) || Input.GetKeyDown(KeyCode.Underscore) || Input.GetKeyDown(KeyCode.KeypadMinus))
+        {
+            Time.timeScale -= 0.1f;
+        }
+
+        textMesh.text = "TIMESCALE " + ((int)(Time.timeScale * 100f));
+
+	}
+}
